871d92f21c8d800c9a20bb57a43dee6cf44ff58e
Suraj Yadav <ersuraj097@gmail.com>
2025-07-11 11:25:31 +0530

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,13 +9,15 @@ const About = () => {
 
             <div className="about-heading" data-aos="flip-left">
                 <strong>About me</strong>
-                <h3>Designing with passion for problem sovling</h3>
+                // <h3>Designing with passion for problem sovling</h3>
+                <h3>Engineering Intelligence with a Passion for Problem Solving</h3>
+
                 <a href="tel:9792440259" className='btn-link'><i class="fa-solid fa-phone fa-beat-fade"></i>+91 9792440259</a>
             </div>
             
             <div className="about-details" data-aos="flip-right">
                 <p>
-                I’m a designer with a passion for problem-solving. I focus on creating intuitive, user-centered designs that not only look great but also provide effective solutions. My goal is to blend creativity with functionality to bring ideas to life.
+               I'm an AI engineer passionate about solving problems by building intelligent, user-focused solutions that combine data, algorithms, and real-world impact.
                 </p>
            
             </div>
